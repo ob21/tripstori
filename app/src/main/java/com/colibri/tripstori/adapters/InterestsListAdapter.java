c11c1174233538b1010885d903a44fcf64fb666a
olivier briand <olivier.briand@orange.com>
2015-06-22 23:41:26 +0200

    Set different item views depending on the interest type

--- app/src/main/java/com/colibri/tripstori/adapters/InterestsListAdapter.java
+++ app/src/main/java/com/colibri/tripstori/adapters/InterestsListAdapter.java
@@ -25,6 +25,9 @@ public class InterestsListAdapter extends BaseAdapter {
             "http://developer.android.com/images/training/system-ui.png";
     private static final String IMAGE1_URL =
             "http://www.setgoogle.com/images/domain/homepage/google_icon/google_play.png";
+    private static final int VIEW_TYPE_NONE = 0;
+    private static final int VIEW_TYPE_NOTE = 1;
+    private static final int VIEW_TYPE_GEO = 2;
 
     private ArrayList<Interest> mInterests = new ArrayList<>();
     private LayoutInflater mInflater;
@@ -53,43 +56,84 @@ public class InterestsListAdapter extends BaseAdapter {
         return 0;
     }
 
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mInterests.get(position).getType() == Interest.Type.NOTE) {
+            return VIEW_TYPE_NOTE;
+        } else if (mInterests.get(position).getType() == Interest.Type.GEO) {
+            return VIEW_TYPE_GEO;
+        } else {
+            return VIEW_TYPE_NONE;
+        }
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View view;
-        ViewHolder holder;
-        if(convertView == null) {
-            view = mInflater.inflate(R.layout.item_interest, parent, false);
-            holder = new ViewHolder();
-            holder.image = (NetworkImageView)view.findViewById(R.id.item_interest_niv);
-            holder.id = (TextView)view.findViewById(R.id.item_interest_id);
-            holder.title = (TextView)view.findViewById(R.id.item_interest_title);
-            view.setTag(holder);
-        } else {
-            view = convertView;
-            holder = (ViewHolder)view.getTag();
-        }
+        ViewNoteHolder noteHolder = null;
+        ViewGeoHolder geoHolder = null;
 
-        Interest interest = mInterests.get(position);
-        if(position % 2 == 0) {
-            holder.image.setImageUrl(IMAGE_URL, VolleyManager.getImageLoader());
+        int type = getItemViewType(position);
+
+        if(type == VIEW_TYPE_NOTE) {
+
+            if (convertView == null) {
+                view = mInflater.inflate(R.layout.item_note_interest, parent, false);
+                noteHolder = new ViewNoteHolder();
+                noteHolder.image = (NetworkImageView) view.findViewById(R.id.item_interest_niv);
+                noteHolder.id = (TextView) view.findViewById(R.id.item_interest_id);
+                noteHolder.title = (TextView) view.findViewById(R.id.item_interest_title);
+                view.setTag(noteHolder);
+            } else {
+                view = convertView;
+                noteHolder = (ViewNoteHolder) view.getTag();
+            }
+
+            Interest interest = mInterests.get(position);
+
+            noteHolder.id.setText(String.valueOf(interest.getId()));
+            noteHolder.title.setText(interest.getTitle() + " - " + interest.getType());
+            noteHolder.text.append(interest.getText());
+
         } else {
-            holder.image.setImageUrl(IMAGE1_URL, VolleyManager.getImageLoader());
-        }
-        holder.id.setText(String.valueOf(interest.getId()));
-        holder.title.setText(interest.getTitle() + " - " + interest.getType());
-        if(interest.getType()== Interest.Type.GEO) {
-            holder.title.append(" - "+interest.getLongitude()+", "+interest.getLatitude());
-        }
-        if(interest.getType()== Interest.Type.NOTE) {
-            holder.title.append(" - "+interest.getText());
+
+            if (convertView == null) {
+                view = mInflater.inflate(R.layout.item_geo_interest, parent, false);
+                geoHolder = new ViewGeoHolder();
+                geoHolder.image = (NetworkImageView) view.findViewById(R.id.item_interest_niv);
+                geoHolder.id = (TextView) view.findViewById(R.id.item_interest_id);
+                geoHolder.title = (TextView) view.findViewById(R.id.item_interest_title);
+                view.setTag(geoHolder);
+            } else {
+                view = convertView;
+                geoHolder = (ViewGeoHolder) view.getTag();
+            }
+
+            Interest interest = mInterests.get(position);
+
+            geoHolder.id.setText(String.valueOf(interest.getId()));
+            geoHolder.title.setText(interest.getTitle() + " - " + interest.getType());
+            geoHolder.longitude.setText(String.valueOf(interest.getLongitude()));
+            geoHolder.latitude.setText(String.valueOf(interest.getLatitude()));
+
         }
 
         return view;
     }
 
-    private class ViewHolder {
+    private class ViewNoteHolder {
         public NetworkImageView image;
         public TextView id;
         public TextView title;
+        public TextView text;
+    }
+
+    private class ViewGeoHolder {
+        public NetworkImageView image;
+        public TextView id;
+        public TextView title;
+        public TextView longitude;
+        public TextView latitude;
     }
 }
