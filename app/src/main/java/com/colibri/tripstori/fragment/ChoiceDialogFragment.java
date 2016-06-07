044827c836561b8394f0350f1ff7ed4f2649fe3d
OPOB7414 <olivier.briand@orange.com>
2016-06-07 11:34:29 +0200

    Customize items in dialog when creating an new interest

--- app/src/main/java/com/colibri/tripstori/fragment/ChoiceDialogFragment.java
+++ app/src/main/java/com/colibri/tripstori/fragment/ChoiceDialogFragment.java
@@ -2,10 +2,22 @@ package com.colibri.tripstori.fragment;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.content.res.ResourcesCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import com.colibri.tripstori.R;
 
 import java.util.List;
 
@@ -26,7 +38,7 @@ public class ChoiceDialogFragment extends TSDialogFragment {
 
         AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
 
-        dialog.setTitle("Choisissez ce que vous voulez notez");
+        dialog.setTitle("Ajouter un intérêt");
         dialog.setPositiveButton("Annuler", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -34,12 +46,68 @@ public class ChoiceDialogFragment extends TSDialogFragment {
             }
         });
 
-        List<String> list = (List<String>)bundle.get(DATA);
+        final List<String> items = (List<String>)bundle.get(DATA);
         int position = bundle.getInt(SELECTED);
 
-        CharSequence[] cs = list.toArray(new CharSequence[list.size()]);
-        dialog.setItems(cs, (DialogInterface.OnClickListener) getActivity());
+
+        ListAdapter adapter = new ArrayAdapter<String>(
+                getActivity(), R.layout.dialog_item, items) {
+
+            ViewHolder holder;
+
+            class ViewHolder {
+                ImageView icon;
+                TextView title;
+            }
+
+            public View getView(int position, View convertView,
+                                ViewGroup parent) {
+                final LayoutInflater inflater = (LayoutInflater) getActivity()
+                        .getSystemService(
+                                Context.LAYOUT_INFLATER_SERVICE);
+
+                if (convertView == null) {
+                    convertView = inflater.inflate(
+                            R.layout.dialog_item, null);
+
+                    holder = new ViewHolder();
+                    holder.icon = (ImageView) convertView
+                            .findViewById(R.id.dialog_item_iv);
+                    holder.title = (TextView) convertView
+                            .findViewById(R.id.dialog_item_tv);
+                    convertView.setTag(holder);
+                } else {
+                    // view already defined, retrieve view holder
+                    holder = (ViewHolder) convertView.getTag();
+                }
+
+                Drawable drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.note_icon, null);
+                switch(position) {
+                    case 0:
+                        drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.note_icon, null);
+                        break;
+                    case 1:
+                        drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.geo_icon, null);
+                        break;
+                    case 2:
+                        drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.image_icon, null);
+                        break;
+                    case 3:
+                        drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.web_icon, null);
+                        break;
+                }
+
+                holder.title.setText(items.get(position));
+                holder.icon.setImageDrawable(drawable);
+
+                return convertView;
+            }
+        };
+
+        dialog.setAdapter(adapter, (DialogInterface.OnClickListener) getActivity());
 
         return dialog.create();
     }
+
 }
+
